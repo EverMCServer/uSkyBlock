68955bd7471c601fa56bd199d9fd2ae80c89a5ce
djytw <djytw@live.com>
2019-05-14 00:30:54 +0800

    Add /is greeting and /is farewell

---
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/command/island/GreetingCommand.java
@@ -0,0 +1,39 @@
+package us.talabrek.ultimateskyblock.command.island;
+
+import dk.lockfuglsang.minecraft.command.AbstractCommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import us.talabrek.ultimateskyblock.api.IslandInfo;
+import us.talabrek.ultimateskyblock.uSkyBlock;
+
+import java.util.Map;
+
+import static dk.lockfuglsang.minecraft.po.I18nUtil.marktr;
+import static dk.lockfuglsang.minecraft.po.I18nUtil.tr;
+
+public class GreetingCommand extends AbstractCommand {
+    private final uSkyBlock plugin;
+
+    public GreetingCommand(uSkyBlock plugin) {
+        super("greeting", "usb.island.create", "?msg", marktr("Change greeting message of your island"));
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String alias, Map<String, Object> data, String... args) {
+        Player player = (Player)sender;
+        IslandInfo islandInfo = uSkyBlock.getInstance().getIslandInfo(player);
+        System.out.println(islandInfo.getName() + "island");
+        System.out.println(args[0]);
+        String str="";
+        for (String arg:args){
+            str+=arg+" ";
+        }
+        String cmd = "console:rg flag -w world_skyland " + islandInfo.getName() + "island greeting "+str;
+        uSkyBlock.getInstance().execCommand(player, cmd,false);
+
+        str = str.replaceAll("&(?=[0-9a-fA-FkKlLmMnNoOrR])", "\u00a7");
+        player.sendMessage(tr("\u00a7bYour island greeting message has changed to: \u00a7r")+str);
+        return true;
+    }
+}
