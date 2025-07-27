932dcf7810335197f6c4c05180029ee751874d8c
Cong0707 <49234756+Cong0707@users.noreply.github.com>
2025-07-27 13:26:50 +0800

    去除潜影贝转换 去除phantom_protection

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/SpawnEvents.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/SpawnEvents.java
@@ -2,11 +2,7 @@ package us.talabrek.ultimateskyblock.event;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Registry;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Biome;
 import org.bukkit.entity.*;
 import org.bukkit.event.Cancellable;
@@ -22,15 +18,14 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import us.talabrek.ultimateskyblock.api.IslandInfo;
 import us.talabrek.ultimateskyblock.handler.WorldGuardHandler;
-import us.talabrek.ultimateskyblock.player.PlayerInfo;
 import us.talabrek.ultimateskyblock.uSkyBlock;
-import us.talabrek.ultimateskyblock.util.LogUtil;
+import us.talabrek.ultimateskyblock.util.LocationUtil;
 
-import java.util.*;
-import java.util.logging.Level;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
 
 import static dk.lockfuglsang.minecraft.po.I18nUtil.tr;
-import static us.talabrek.ultimateskyblock.util.LogUtil.log;
 
 /**
  * Responsible for controlling spawns on uSkyBlock islands.
@@ -39,7 +34,6 @@ import static us.talabrek.ultimateskyblock.util.LogUtil.log;
 public class SpawnEvents implements Listener {
     private static final Set<Action> RIGHT_CLICKS = Set.of(Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK);
 
-    private HashMap<String, Boolean> newIsland = new HashMap<>();
     private final uSkyBlock plugin;
 
     private boolean phantomsInOverworld;
@@ -94,12 +88,6 @@ public class SpawnEvents implements Listener {
         }
     }
 
-    private int fastpos(int pos){
-        pos+=64;
-        return (pos<0)?((pos+1)/128):(pos/128);
-    }
-
-
     @EventHandler(ignoreCancelled = true)
     public void onCreatureSpawn(CreatureSpawnEvent event) {
         if (event == null || !plugin.getWorldManager().isSkyAssociatedWorld(event.getLocation().getWorld())) {
@@ -108,121 +96,24 @@ public class SpawnEvents implements Listener {
         if (event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.SPAWNER_EGG)) {
             return; // Allow it, the above method would have blocked it if it should be blocked.
         }
-        if (event.getEntity() instanceof Phantom) {
-            String island = fastpos(event.getLocation().getBlockX()) + "," + fastpos(event.getLocation().getBlockZ());
-
-            if(newIsland.get(island) == null){
-                IslandInfo is = plugin.getIslandInfo(event.getLocation());
-                if(is != null){
-                    PlayerInfo pi = plugin.getPlayerInfo(is.getLeader());
-                    if (pi != null && pi.checkChallenge("page1finished")==0){
-                        // newbie protection
-                        event.setCancelled(true);
-                        pi = null; is = null;
-                        newIsland.put(island, true);
-                        log(Level.INFO, "Add inf Phantom Protection: "+island+" : protect");
-                        return;
-                    }
-                    pi = null; is = null;
-                    newIsland.put(island, false);
-                    log(Level.INFO, "Add inf Phantom Protection: "+island+" : Not-protect");
-                }
-            }else{
-                if (newIsland.get(island) == true){
-                    event.setCancelled(true);
-                    return;
-                }
-            }
-        }
-        if (event.getEntity() instanceof WaterMob) {
-            Random r = new Random();
-            if (r.nextInt(20) != 0){
-                event.setCancelled(true);
-                return;
-            }
-        }
-        if (event.getEntity() instanceof ArmorStand){
-            return;
-        }
         checkLimits(event, event.getEntity().getType(), event.getLocation());
         if (event.getEntity() instanceof WaterMob) {
             Location loc = event.getLocation();
-            if(doPrismarineRoof(loc))
+            if (isDeepOceanBiome(loc) && isPrismarineRoof(loc)) {
+                loc.getWorld().spawnEntity(loc, EntityType.GUARDIAN);
                 event.setCancelled(true);
-        }
-        if (!event.isCancelled() && event.getEntity() instanceof Enderman) {
-            Location loc = event.getLocation();
-
-            if(isPurpurFloor(loc)) {
-                if (isEndBiome(loc)) {
-                    Random r = new Random();
-                    if (r.nextInt(10) == 0) {
-                        loc.getWorld().spawnEntity(loc, EntityType.SHULKER);
-                        event.setCancelled(true);
-                    }
-                } else {
-                    loc.getWorld().spawnEntity(loc, EntityType.SHULKER);
-                    event.setCancelled(true);
-                }
             }
         }
     }
 
-    private boolean isPurpurFloor(Location loc){
-        List<Material> purpurBlocks = Arrays.asList(Material.PURPUR_BLOCK, Material.PURPUR_PILLAR, Material.PURPUR_SLAB);
-        loc.setY(loc.getY()-1);
-        boolean ret=purpurBlocks.contains(loc.getBlock().getType());
-        loc.setY(loc.getY()+1);
-        return ret;
+    private boolean isPrismarineRoof(Location loc) {
+        Collection<Material> prismarineBlocks = Set.of(Material.PRISMARINE, Material.PRISMARINE_BRICKS, Material.DARK_PRISMARINE);
+        return prismarineBlocks.contains(LocationUtil.findRoofBlock(loc).getType());
     }
 
-    private boolean doPrismarineRoof(Location loc) {
-        List<Material> prismarineBlocks = Arrays.asList(Material.PRISMARINE, Material.PRISMARINE_BRICKS, Material.DARK_PRISMARINE);
-        Location tloc = loc.clone();
-        if(tloc.getBlockY()<47 || tloc.getBlockY()>64)
-            return false;
-        while(tloc.getBlockY()<=70){
-            if (tloc.getBlock().getType() == Material.WATER){
-                tloc.add(0,1,0);
-            }else{
-                if(prismarineBlocks.contains(tloc.getBlock().getType())){
-                    Random r = new Random();
-                    if (r.nextInt(5) == 0){
-                        if(r.nextInt(1000) == 0){
-                            if(r.nextInt(1000)==0){
-                                Drowned drowned= (Drowned) loc.getWorld().spawnEntity(loc, EntityType.DROWNED);
-                                drowned.getEquipment().setItemInMainHand(new ItemStack(Material.TRIDENT));
-                                LogUtil.log(Level.INFO, java.time.Clock.systemUTC().instant()+" "+plugin.getIslandInfo(loc).getLeader()+" RANDOM TRIDENT");
-                            }
-                            else{
-                                Drowned drowned= (Drowned) loc.getWorld().spawnEntity(loc, EntityType.DROWNED);
-                                if(drowned.getEquipment().getItemInMainHand().equals(new ItemStack(Material.TRIDENT))){
-                                    LogUtil.log(Level.INFO,java.time.Clock.systemUTC().instant()+" "+plugin.getIslandInfo(loc).getLeader()+" DROWNED TRIDENT");
-                                }
-                                else{
-                                    LogUtil.log(Level.INFO,java.time.Clock.systemUTC().instant()+" "+plugin.getIslandInfo(loc).getLeader()+" DROWNED NO TRIDENT");
-                                }
-                            }
-                        }
-                        else{
-                            loc.getWorld().spawnEntity(loc, EntityType.GUARDIAN);
-                        }
-                    }
-                    return true;
-                }else if(tloc.getBlock().getType() == Material.SEA_LANTERN){
-                    Random r = new Random();
-                    if (r.nextInt(50) == 0)
-                        loc.getWorld().spawnEntity(loc, EntityType.ELDER_GUARDIAN);
-                    return true;
-                }
-                return false;
-            }
-        }
-        return false;
-    }
-
-    private boolean isEndBiome(Location loc) {
-        return loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockZ())==Biome.THE_END;
+    private boolean isDeepOceanBiome(Location loc) {
+        Collection<Biome> deepOceans = Set.of(Biome.DEEP_OCEAN, Biome.DEEP_COLD_OCEAN, Biome.DEEP_FROZEN_OCEAN, Biome.DEEP_LUKEWARM_OCEAN);
+        return deepOceans.contains(loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
     }
 
     private void checkLimits(Cancellable event, EntityType entityType, Location location) {
@@ -248,7 +139,7 @@ public class SpawnEvents implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPhantomSpawn(CreatureSpawnEvent event) {
         if (!(event.getEntity() instanceof Phantom) ||
-                event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) {
+            event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) {
             return;
         }
 
@@ -264,6 +155,7 @@ public class SpawnEvents implements Listener {
 
     /**
      * Changes the setting that allows Phantoms to spawn in the overworld. Used for testing purposes.
+     *
      * @param state True/enabled means spawning is allowed, false disallowed.
      */
     void setPhantomsInOverworld(boolean state) {
@@ -272,6 +164,7 @@ public class SpawnEvents implements Listener {
 
     /**
      * Changes the setting that allows Phantoms to spawn in the nether. Used for testing purposes.
+     *
      * @param state True/enabled means spawning is allowed, false disallowed.
      */
     void setPhantomsInNether(boolean state) {
