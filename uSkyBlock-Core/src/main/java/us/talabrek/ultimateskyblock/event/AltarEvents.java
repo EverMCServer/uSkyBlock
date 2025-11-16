509cbf8eecabb64a366cea63d67c56c0ec03d960
GJ <740646495@qq.com>
2025-11-16 23:54:50 +0800

    feat: 供奉富饶之祭坛，随机获得物品

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/AltarEvents.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/AltarEvents.java
@@ -20,17 +20,22 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
+import us.talabrek.ultimateskyblock.hook.economy.EconomyHook;
+import us.talabrek.ultimateskyblock.island.IslandInfo;
 import us.talabrek.ultimateskyblock.player.PlayerInfo;
 import us.talabrek.ultimateskyblock.uSkyBlock;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static dk.lockfuglsang.minecraft.po.I18nUtil.tr;
 
 @Singleton
 public class AltarEvents implements Listener {
     private final uSkyBlock plugin;
+    private static final Random RANDOM = new Random();
 
     enum AltarType {
         NONE,
@@ -47,12 +52,13 @@ public class AltarEvents implements Listener {
     static public ItemStack stoneOfPeace() {
         ItemStack item = new ItemStack(Material.LAPIS_LAZULI);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(tr("和平之石"));
+        meta.setDisplayName(tr("\u00a7l\u00a79和平之石"));
         meta.addEnchant(Enchantment.PROTECTION, 10, true);
         List<String> lore = new ArrayList<>();
-        lore.add(tr("和平之石"));
-        lore.add(tr("消耗品，为副手的护甲增加一级保护附魔"));
-        lore.add(tr("消耗数量等于目标保护等级，最多升级到保护10"));
+        lore.add(tr("\u00a7l\u00a79和平之石"));
+        lore.add(tr("\u00a7l\u00a7e消耗品，为副手物品加一级保护"));
+        lore.add(tr("\u00a7l\u00a7e消耗数等于目标保护等级"));
+        lore.add(tr("\u00a7l\u00a7e最多升级到保护10"));
         meta.setLore(lore);
         item.setItemMeta(meta);
         return item;
@@ -114,6 +120,120 @@ public class AltarEvents implements Listener {
     }
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onAltarInteract(final PlayerInteractEvent event) {
+        // 当玩家右键点击祭坛时，提示其类型和计数器
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        if (!plugin.getWorldManager().isSkyWorld(player.getWorld())) {
+            return;
+        }
+        if (block != null &&
+            block.getType() == Material.REINFORCED_DEEPSLATE &&
+            event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            // 如果此方块不是玩家的岛屿，则提示
+            IslandInfo islandInfo = plugin.getIslandInfo(block.getLocation());
+            if (islandInfo == null || !islandInfo.isMember(player)) {
+                player.sendMessage(tr("\u00a7c你只能在自己的岛屿上使用祭坛。"));
+                return;
+            }
+            PersistentDataContainer pdc = block.getChunk().getPersistentDataContainer();
+            Integer altarTypeOrdinal = pdc.get(getKeyAltarType(block), PersistentDataType.INTEGER);
+            Long altarCounter = pdc.get(getKeyAltarCounter(block), PersistentDataType.LONG);
+            if (altarTypeOrdinal != null && altarCounter != null) {
+                AltarType altarType = AltarType.values()[altarTypeOrdinal];
+                // 检查上面是信标
+                Block up = block.getRelative(0, 1, 0);
+                if (up.getType() != Material.BEACON) {
+                    player.sendMessage(tr("\u00a7c祭坛上方必须有一个信标才能使用。"));
+                    return;
+                }
+
+                switch (altarType) {
+                    case HARVEST -> {
+                        // player.sendMessage(tr("\u00a7a这是一个 \u00a7l\u00a72收获之祭坛\u00a7a，已被使用了 \u00a7l\u00a73%d \u00a7a次。", altarCounter));
+                    }
+                    case WAR -> {
+                        // player.sendMessage(tr("\u00a7a这是一个 \u00a7l\u00a74战争之祭坛\u00a7a，已被使用了 \u00a7l\u00a73%d \u00a7a次。", altarCounter));
+                    }
+                    case WEALTH -> {
+                        player.sendMessage(tr("\u00a7a这是一个 \u00a7l\u00a76富饶之祭坛\u00a7a，共奉献了 \u00a7l\u00a73%d \u00a7a金币。", altarCounter));
+                        // 玩家可奉献1个钻石+1000金币
+                        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+                        if (itemInHand.getType() == Material.DIAMOND) {
+                            EconomyHook hook = plugin.getHookManager().getEconomyHook().get();
+                            double money = hook.getBalance(player);
+                            if (money >= 1000) {
+                                // 扣除物品和金币
+                                itemInHand.setAmount(itemInHand.getAmount() - 1);
+                                hook.withdrawPlayer(player, 1000);
+                                // 增加祭坛计数器
+                                altarCounter += 1000;
+                                pdc.set(getKeyAltarCounter(block), PersistentDataType.LONG, altarCounter);
+                                player.sendMessage(tr("\u00a7a你向富饶之祭坛奉献了 \u00a7l\u00a73一颗钻石 \u00a7a和 \u00a7l\u00a73 1000g\u00a7a。"));
+                                player.getWorld().playSound(block.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1.0f, 1.0f);
+                                wealthReward(player);
+                            } else {
+                                player.sendMessage(tr("\u00a7c奉献需要一颗钻石和1000g。"));
+                                return;
+                            }
+                        } else {
+                            // 否则，提示玩家需要的物品
+                            player.sendMessage(tr("\u00a7c奉献需要一颗钻石和1000g。"));
+                        }
+                    }
+                    case SOUL -> {
+                        // player.sendMessage(tr("\u00a7a这是一个 \u00a7l\u00a75灵魂之祭坛\u00a7a，已被使用了 \u00a7l\u00a73%d \u00a7a次。", altarCounter));
+                    }
+                    case EXPLORATION -> {
+                        // player.sendMessage(tr("\u00a7a这是一个 \u00a7l\u00a79探索之祭坛\u00a7a，已被使用了 \u00a7l\u00a73%d \u00a7a次。", altarCounter));
+                    }
+                    default -> {
+                        return;
+                    }
+                }
+            } else {
+                return;
+            }
+        }
+    }
+
+    private void wealthReward(Player player) {
+        double roll = RANDOM.nextDouble();
+        // 3% 和平之石，5% 永久之石，2% 财富之石
+        // 钻石 蓝冰 绿宝石块 金块 各10%
+        // 钻石块 远古残骸 下界合金 各1%
+        // 下界合金块 0.1%
+        // 青金石块 红石块 平分其余
+        if (roll < 0.03) {
+            player.getWorld().dropItemNaturally(player.getLocation(), stoneOfPeace());
+        } else if (roll < 0.08) {
+            player.getWorld().dropItemNaturally(player.getLocation(), stoneOfEternity());
+        } else if (roll < 0.10) {
+            player.getWorld().dropItemNaturally(player.getLocation(), stoneOfWealth());
+        } else if (roll < 0.20) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.DIAMOND, 1));
+        } else if (roll < 0.30) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.BLUE_ICE, 1));
+        } else if (roll < 0.40) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.EMERALD_BLOCK, 1));
+        } else if (roll < 0.50) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.GOLD_BLOCK, 1));
+        } else if (roll < 0.51) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.DIAMOND_BLOCK, 1));
+        } else if (roll < 0.52) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.ANCIENT_DEBRIS, 1));
+        } else if (roll < 0.53) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.NETHERITE_INGOT, 1));
+        } else if (roll < 0.531) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.NETHERITE_BLOCK, 1));
+        } else if (roll < 0.7655) {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.LAPIS_BLOCK, 1));
+        } else {
+            player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.REDSTONE_BLOCK, 1));
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onBuildAltar(final PlayerInteractEvent event) {
         // 当右键点击一个下界合金块时，检查是否允许建造祭坛
         Player player = event.getPlayer();
@@ -129,14 +249,15 @@ public class AltarEvents implements Listener {
                 return;
             }
             // 如果此方块不是玩家的岛屿，则提示
-            if (!plugin.getIslandInfo(block.getLocation()).isMember(player)) {
-                player.sendMessage(tr("\u00a7cYou can only build altars on your own island."));
+            IslandInfo islandInfo = plugin.getIslandInfo(block.getLocation());
+            if (islandInfo == null || !islandInfo.isMember(player)) {
+                player.sendMessage(tr("\u00a7c你只能在自己的岛屿上建造祭坛。"));
                 return;
             }
             PlayerInfo playerInfo = plugin.getPlayerInfo(player);
             // 如果玩家未完成进度，则提示
             if (plugin.getChallengeLogic().checkChallenge(playerInfo, "advanced") == 0) {
-                player.sendMessage(tr("\u00a7cYou can only build altars after completing the 'Advanced' challenge."));
+                player.sendMessage(tr("\u00a7c完成'海岛进阶'挑战后，才能建造祭坛。"));
                 return;
             }
             // 检查玩家主手的物品
@@ -151,7 +272,7 @@ public class AltarEvents implements Listener {
                             // 扣除物品和金币
                             itemInHand.setAmount(itemInHand.getAmount() - 5);
                             hook.withdrawPlayer(player, 50000);
-                            player.sendMessage(tr("\u00a7aYou have successfully built Altar of Wealth!"));
+                            plugin.getServer().broadcastMessage(String.format("\u00a7e%s \u00a7a在他们的岛屿上建造了 富饶之祭坛!", player.getName()));
                             buildAltar(block, AltarType.WEALTH);
                             // 产生音效
                             player.getWorld().playSound(block.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 1.0f, 1.0f);
@@ -160,17 +281,17 @@ public class AltarEvents implements Listener {
                             block.getWorld().dropItemNaturally(player.getLocation(), stoneOfEternity());
                             block.getWorld().dropItemNaturally(player.getLocation(), stoneOfWealth());
                         } else {
-                            player.sendMessage(tr("\u00a7cYou need 5 diamonds and 50,000g to build Altar of Wealth."));
+                            player.sendMessage(tr("\u00a7c需要5个钻石和50000g来建造 富饶之祭坛。"));
                             return;
                         }
                     });
                 } else {
-                    player.sendMessage(tr("\u00a7cYou need 5 diamonds and 50,000g to build Altar of Wealth."));
+                    player.sendMessage(tr("\u00a7c需要5个钻石和50000g来建造 富饶之祭坛。"));
                     return;
                 }
             } else {
                 // 否则，提示玩家需要的物品
-                player.sendMessage(tr("\u00a7c"));
+                player.sendMessage(tr("\u00a7c需要5个钻石和50000g来建造 富饶之祭坛。"));
             }
         }
     }
