1a748405b64dc2c191353cf71b469a91485ed4e2
GJ <740646495@qq.com>
2025-11-17 01:32:32 +0800

    bugfix: equals->contains

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/AltarEvents.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/AltarEvents.java
@@ -137,11 +137,11 @@ public class AltarEvents implements Listener {
             String firstLore = itemInHand.getItemMeta().getLore().getFirst();
             plugin.getLogger().info(String.format("Item lore first line: %s", firstLore));
             plugin.getLogger().info(String.format("Event action: %s", event.getAction().name()));
-            if (firstLore.equals("\u00a7l\u00a79和平之石")) {
+            if (firstLore.contains("和平之石")) {
                 tryUseStoneOfPeace(player, itemInHand, event);
-            } else if (firstLore.equals("\u00a7l\u00a7b永久之石")) {
+            } else if (firstLore.contains("永久之石")) {
                 tryUseStoneOfEternity(player, itemInHand, event);
-            } else if (firstLore.equals("\u00a7l\u00a76财富之石")) {
+            } else if (firstLore.contains("财富之石")) {
                 tryUseStoneOfWealth(player, itemInHand, event);
             }
         }
